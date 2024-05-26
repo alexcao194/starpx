699424b110c9981d2e4a1c09fb922396e5c6d71e
duccv <duc.cv@zinza.com.vn>
2024-05-26 16:19:06 +0700

    feat: create Apollo Client

---
+++ app/src/main/java/com/alexcao/starpx/repository/ApolloClient.kt
@@ -0,0 +1,7 @@
+package com.alexcao.starpx.repository
+
+import com.apollographql.apollo3.ApolloClient
+
+val apolloClient = ApolloClient.Builder()
+    .serverUrl("YOUR_GRAPHQL_ENDPOINT")
+    .build()
