b1294f77bcd82513e00b6a41431beb0a72a7568f
duccv <duc.cv@zinza.com.vn>
2024-05-26 15:37:39 +0700

    feat(login): create viewmodel

---
+++ app/src/main/java/com/alexcao/starpx/screen/login/LoginViewModel.kt
@@ -0,0 +1,27 @@
+package com.alexcao.starpx.screen.login
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+
+class LoginViewModel : ViewModel() {
+    var username by mutableStateOf("")
+        private set
+
+    var password by mutableStateOf("")
+        private set
+
+    fun onUsernameChange(newUsername: String) {
+        username = newUsername
+    }
+
+    fun onPasswordChange(newPassword: String) {
+        password = newPassword
+    }
+
+
+    fun login() {
+        // Perform login logic here
+    }
+}
