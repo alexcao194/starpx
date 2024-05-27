1e4a44f7807fc0e5d1f2761cc9e596bff2b22c9b
alexcao <alexcao194@gmail.com>
2024-05-27 05:17:11 -0700

    feat: add apollo schema

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,10 +58,13 @@ dependencies {
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.apollo.android.support)
-    implementation(libs.apollo.runtime)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.navigation.compose)
+
+    implementation(libs.apollo.android.support)
+    implementation(libs.apollo.runtime)
+
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -69,4 +72,10 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+}
+
+apollo {
+    service("service") {
+        packageName = "com.alexcao.starpx"
+    }
 }
