b56fc753e0d10ab2c62065bcbb93b798dba50fb8
alexcao <alexcao194@gmail.com>
2024-05-27 09:01:06 -0700

    feat: link HomeViewModel into HomeScreen

--- app/src/main/java/com/alexcao/starpx/screen/home/HomeScreen.kt
+++ app/src/main/java/com/alexcao/starpx/screen/home/HomeScreen.kt
@@ -1,22 +1,40 @@
 package com.alexcao.starpx.screen.home
 
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 
 @Composable
 fun HomeScreen(
-    navController: NavController
+    navController: NavController,
+    viewModel: HomeViewModel = hiltViewModel()
 ) {
-    Scaffold {
-        Box(
-            modifier = Modifier.padding(it)
+    val homeUiState = viewModel.homeUiState.collectAsState()
+    val data = homeUiState.value.data
+    Scaffold {padding ->
+        LazyVerticalGrid(
+            modifier = Modifier.padding(padding),
+            columns = GridCells.Adaptive(minSize = 128.dp)
         ) {
-            Text(text = "Home Screen")
+            items(data.size) {index ->
+                ImageCell(thumbnail = data[index].thumbnail)
+            }
         }
     }
+}
+
+@Composable
+fun ImageCell(
+    modifier: Modifier = Modifier,
+    thumbnail: String
+) {
+    Text(text = thumbnail, modifier = modifier.padding(16.dp))
 }
