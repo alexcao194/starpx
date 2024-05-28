84b25bd8ee454840676f5b33c55bea1546e08a52
alexcao <alexcao194@gmail.com>
2024-05-28 07:24:56 -0700

    feat(LoginScreen): add circular progress indicator overlap

--- app/src/main/java/com/alexcao/starpx/screen/login/LoginScreen.kt
+++ app/src/main/java/com/alexcao/starpx/screen/login/LoginScreen.kt
@@ -1,7 +1,9 @@
 package com.alexcao.starpx.screen.login
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,6 +13,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
@@ -20,6 +23,7 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
@@ -52,63 +56,79 @@ fun LoginScreen(
         }
     }
 
+    val loginUiState = viewModel.loginUiState.collectAsState()
+
     Scaffold { padding ->
-        Column(
-            modifier = Modifier
-                .padding(padding)
-                .padding(16.dp)
-                .fillMaxSize()
-                .imePadding(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
+        Box(
+            modifier = Modifier.padding(padding)
         ) {
-            Image(
-                modifier = Modifier.size(240.dp),
-                painter = painterResource(id = R.drawable.ic_launcher_foreground),
-                contentDescription = "App Icon"
-            )
-            Text(
-                text = stringResource(id = R.string.app_name),
-                style = MaterialTheme.typography.displayMedium,
-            )
-            Spacer(modifier = Modifier.size(16.dp))
-            OutlinedTextField(
-                modifier = Modifier.fillMaxWidth(),
-                value = viewModel.username,
-                keyboardOptions = KeyboardOptions(
-                    keyboardType = KeyboardType.Email,
-                    imeAction = ImeAction.Next
-                ),
-                onValueChange = { username ->
-                    viewModel.onUsernameChange(username)
-                },
-                label = {
-                    Text(text = stringResource(id = R.string.username_label))
-                },
-            )
-            Spacer(modifier = Modifier.size(16.dp))
-            OutlinedTextField(
-                modifier = Modifier.fillMaxWidth(),
-                value = viewModel.password,
-                onValueChange = { password ->
-                    viewModel.onPasswordChange(password)
-                },
-                keyboardOptions = KeyboardOptions(
-                    keyboardType = KeyboardType.Password,
-                    imeAction = ImeAction.Done
-                ),
-                label = {
-                    Text(text = stringResource(R.string.password_label))
-                }
-            )
-            Spacer(modifier = Modifier.size(16.dp))
-            Button(
-                modifier = Modifier.fillMaxWidth(),
-                onClick = {
-                    viewModel.login()
-                }
+            Column(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxSize()
+                    .imePadding(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
             ) {
-                Text(text = stringResource(R.string.login_button))
+                Image(
+                    modifier = Modifier.size(240.dp),
+                    painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                    contentDescription = "App Icon"
+                )
+                Text(
+                    text = stringResource(id = R.string.app_name),
+                    style = MaterialTheme.typography.displayMedium,
+                )
+                Spacer(modifier = Modifier.size(16.dp))
+                OutlinedTextField(
+                    modifier = Modifier.fillMaxWidth(),
+                    value = viewModel.username,
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Email,
+                        imeAction = ImeAction.Next
+                    ),
+                    onValueChange = { username ->
+                        viewModel.onUsernameChange(username)
+                    },
+                    label = {
+                        Text(text = stringResource(id = R.string.username_label))
+                    },
+                )
+                Spacer(modifier = Modifier.size(16.dp))
+                OutlinedTextField(
+                    modifier = Modifier.fillMaxWidth(),
+                    value = viewModel.password,
+                    onValueChange = { password ->
+                        viewModel.onPasswordChange(password)
+                    },
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Password,
+                        imeAction = ImeAction.Done
+                    ),
+                    label = {
+                        Text(text = stringResource(R.string.password_label))
+                    }
+                )
+                Spacer(modifier = Modifier.size(16.dp))
+                Button(
+                    modifier = Modifier.fillMaxWidth(),
+                    onClick = {
+                        viewModel.login()
+                    }
+                ) {
+                    Text(text = stringResource(R.string.login_button))
+                }
+            }
+            if (loginUiState.value.isLoading) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(Color(0x77000000))
+                    ,
+                    contentAlignment = Alignment.Center
+                ) {
+                    CircularProgressIndicator()
+                }
             }
         }
     }
