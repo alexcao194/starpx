7d7936703377aa44ff0f15b02cd2d04091470186
alexcao <luna207sn@gmail.com>
2024-05-29 08:21:21 -0700

    feat: model serialization

--- app/src/main/java/com/alexcao/starpx/model/ImageSetSummariesResponse.kt
+++ app/src/main/java/com/alexcao/starpx/model/ImageSetSummariesResponse.kt
@@ -1,30 +1,45 @@
-data class ImageSetSummariesResponse(
-    val data: ImageSetSummariesData
+@file:OptIn(ExperimentalSerializationApi::class)
+
+package com.alexcao.starpx.model
+
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.JsonNames
+
+@Serializable
+data class GetImageSetSummariesResponse(
+    val getImageSetSummaries: GetImageSetSummaries
 )
 
-data class ImageSetSummariesData(
-    val getImageSetSummaries: ImageSetSummariesResult
-)
-
-data class ImageSetSummariesResult(
-    val nextToken: String,
+@Serializable
+data class GetImageSetSummaries(
+    val nextToken: String?,
+    @JsonNames("image_sets")
     val imageSets: List<ImageSet>
 )
 
+@Serializable
 data class ImageSet(
     val caption: String,
+    @JsonNames("set_id")
     val setId: String,
     val state: String,
+    @JsonNames("image_detail")
     val imageDetail: ImageDetail
 )
 
+@Serializable
 data class ImageDetail(
+    @JsonNames("full_height")
     val fullHeight: Int,
+    @JsonNames("full_url")
     val fullUrl: String,
+    @JsonNames("full_width")
     val fullWidth: Int,
     val thumbs: Thumbs
 )
 
+@Serializable
 data class Thumbs(
     val xlarge: String,
     val large: String,
