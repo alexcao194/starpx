544a8cda07b61f1eec6c3334c9021b901c975e5f
duccv <duc.cv@zinza.com.vn>
2024-05-31 16:15:07 +0700

    feat: move methods that related to image into AssetUtils

--- app/src/main/java/com/alexcao/starpx/screen/image_detail/ImageDetailScreen.kt
+++ app/src/main/java/com/alexcao/starpx/screen/image_detail/ImageDetailScreen.kt
@@ -32,6 +32,7 @@ import coil.ImageLoader
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import coil.request.SuccessResult
+import com.alexcao.starpx.utls.shareImageFromUrl
 import kotlinx.coroutines.launch
 import java.io.File
 import java.io.FileOutputStream
@@ -81,40 +82,4 @@ fun ImageDetailScreen(
             }
         }
     }
-}
-
-private suspend fun shareImageFromUrl(context: Context, imageUrl: String) {
-    val imageLoader = ImageLoader(context)
-
-    val request = ImageRequest.Builder(context)
-        .data(imageUrl)
-        .allowHardware(false) // Disable hardware bitmaps for drawing to a Canvas
-        .build()
-
-    val result = (imageLoader.execute(request) as SuccessResult).drawable
-    val bitmap = (result as BitmapDrawable).bitmap
-    val uri = saveBitmapToCache(context, bitmap)
-
-    val shareIntent = Intent()
-    shareIntent.action = Intent.ACTION_SEND
-    shareIntent.putExtra(Intent.EXTRA_STREAM, uri)
-    shareIntent.type = "image/*"
-
-    context.startActivity(Intent.createChooser(shareIntent, "Share Image"))
-}
-
-private fun saveBitmapToCache(context: Context, bitmap: Bitmap): Uri {
-    val cachePath = File(context.externalCacheDir, "/")
-    cachePath.mkdirs()
-    val file = File(cachePath, "shared.png")
-    val fileOutputStream: FileOutputStream
-    try {
-        fileOutputStream = FileOutputStream(file)
-        bitmap.compress(Bitmap.CompressFormat.PNG, 100, fileOutputStream)
-        fileOutputStream.close()
-    } catch (e: IOException) {
-        Log.d("SaveBitmapToCache", "saveBitmapToCache: ${e.message}")
-    }
-    Log.d("SaveBitmapToCache", "saveBitmapToCache: ${file.absolutePath}")
-    return FileProvider.getUriForFile(context, "${context.packageName}.provider", file)
 }
