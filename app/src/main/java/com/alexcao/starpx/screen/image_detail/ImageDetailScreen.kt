227ec95af9171ee295cfb6ea457117d367252ec4
duccv <duc.cv@zinza.com.vn>
2024-05-31 16:11:48 +0700

    fix: fix extra data with send action

--- app/src/main/java/com/alexcao/starpx/screen/image_detail/ImageDetailScreen.kt
+++ app/src/main/java/com/alexcao/starpx/screen/image_detail/ImageDetailScreen.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.drawable.BitmapDrawable
 import android.net.Uri
+import android.util.Log
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -94,27 +95,26 @@ private suspend fun shareImageFromUrl(context: Context, imageUrl: String) {
     val bitmap = (result as BitmapDrawable).bitmap
     val uri = saveBitmapToCache(context, bitmap)
 
-    val shareIntent = Intent().apply {
-        action = Intent.ACTION_SEND
-        extras?.putString(Intent.EXTRA_STREAM, uri.toString())
-        type = "image/jpeg"
-        addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
-    }
+    val shareIntent = Intent()
+    shareIntent.action = Intent.ACTION_SEND
+    shareIntent.putExtra(Intent.EXTRA_STREAM, uri)
+    shareIntent.type = "image/*"
 
-    context.startActivity(Intent.createChooser(shareIntent, "Share image via"))
+    context.startActivity(Intent.createChooser(shareIntent, "Share Image"))
 }
 
 private fun saveBitmapToCache(context: Context, bitmap: Bitmap): Uri {
-    val cachePath = File(context.externalCacheDir, "my_images/")
+    val cachePath = File(context.externalCacheDir, "/")
     cachePath.mkdirs()
-    val file = File(cachePath, "shared_image.jpg")
+    val file = File(cachePath, "shared.png")
     val fileOutputStream: FileOutputStream
     try {
         fileOutputStream = FileOutputStream(file)
-        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fileOutputStream)
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, fileOutputStream)
         fileOutputStream.close()
     } catch (e: IOException) {
-        e.printStackTrace()
+        Log.d("SaveBitmapToCache", "saveBitmapToCache: ${e.message}")
     }
+    Log.d("SaveBitmapToCache", "saveBitmapToCache: ${file.absolutePath}")
     return FileProvider.getUriForFile(context, "${context.packageName}.provider", file)
 }
